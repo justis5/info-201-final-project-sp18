6eca0f081b00335c49b6a7cdf012e8538437af75
Justin Sim <justis5@uw.edu>
2018-05-29 15:40:05 -0700

    modify analysis.R file

--- Emily/analysis.R
+++ Emily/analysis.R
@@ -1,18 +1,48 @@
 
-Does higher GDP/capota correlates with a higher average life expectancy?
+# Does higher GDP/capota correlates with a higher average life expectancy?
 library(dplyr)
 library(ggplot2)
 hpi <- read.csv("data/hpi.csv", stringsAsFactors = FALSE)
 
+
+# Rename columns to make variables easier to use
+colnames(hpi)[colnames(hpi) == "Average.Life..Expectancy"] <- 
+  "avg_life_exp"
+colnames(hpi)[colnames(hpi) == "X.GDP.capita...."] <-
+  "gdp_capita"
+
+# Remove formatting from the GDP column
+hpi$gdp_capita <- gsub(",", "", hpi$gdp_capita)
+hpi$gdp_capita <- as.numeric(hpi$gdp_capita)
+
+# Think about a different column
+br = seq(0, 106000,by=21200)
+
+ranges = paste(head(br,-1), br[-1], sep=" - ")
+freq = hist(hpi$gdp_capita, breaks=br, include.lowest=TRUE, plot=FALSE)
+table_df <- data.frame(range = ranges, frequency = freq$counts)
+table_df$low <- c(0, 21200, 42400, 63600, 84800)
+table_df$high <- c(21200, 42400, 63600, 84800, 106000)
+hpi$gdp_category <- cut(hpi$gdp_capita, seq(from = 0, to = 106000, by = 21200))
+
+
+# Source: 
+# https://stackoverflow.com/questions/27839432/
+# how-to-generate-bin-frequency-table-in-r
+
+# https://stackoverflow.com/questions/32975801/
+# specifying-bin-range-values-for-continuous-data-in-r
+
+
+hpi$gdp_under_value <- c(hpi$gdp < 50000)
+
 histogram_AvglifeExp <- function(dataset) {
-  AvglifeExp <- dataset$Average.Life..Expectancy
-  AvglifeExp <- AvglifeExp[!is.na(AvglifeExp)]
-  chart <- ggplot(data = AvglifeExp) +
-    geom_histogram(
+  chart <- ggplot(dataset) +
+    stat_bin(
       mapping = aes(
-        x = Average.Life..Expectancy,
-        fill = X.GDP.capita...PPP..
-      ))
+        x = avg_life_exp,
+        color = gdp_category
+      ), binwidth = 7) +
   labs(
     title = "Histogram of average life expectancy and GDP",
     subtitle = "Info 201 B SP18",
@@ -23,5 +53,4 @@ histogram_AvglifeExp <- function(dataset) {
   return(chart)
 }
 
-gsub("\\$|,", "", X.GDP.capita...PPP.._column)
-as.numeric(X.GDP.capita...PPP.._column)
+
